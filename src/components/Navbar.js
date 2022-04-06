c555ad062da307f8325b0bcf47157e07c3639a28
ezequiel <ps3ezequiel@gmail.com>
2022-04-06 17:24:28 +0200

    first commit of the project

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <>
+        <header className="w-screen flex justify-center  bg-blue-900">
+            <h1 className="text-3xl font-bold underline w-11/12 bg-red-600">
+                Hello world!
+            </h1>
+        </header>
+        </>
+    )
+}
+
+export default Navbar

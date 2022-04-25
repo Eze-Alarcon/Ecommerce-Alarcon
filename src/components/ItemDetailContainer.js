d495ab334b34561f2f7760e32de512f34bb5b1ab
ezequiel <ps3ezequiel@gmail.com>
2022-04-25 21:20:49 +0200

    cleaning

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -11,56 +11,16 @@ function ItemDetailContainer() {
     const { idProduct } = useParams()
 
     useEffect(() => {
-        // fetch(`https://fakestoreapi.com/products/${idProduct}`)
-        // .then(response => response.json())
-        // .then(response => setInfo(response))
-        
-        // .catch(error => setError(error))
-
-        console.log(idProduct)
-
-        /* const fetchFirestone = async () => {            
-            const queryCategory = query(collection(db, "products"), where("id", "==", `${idProduct}`));
-            const queryToDatabase = await getDocs(queryCategory)
-
-            const dataFirestone = queryToDatabase.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-
-            return dataFirestone
-        } */
-
-
-
         const fetchFirestone = async () => {   
             const specificQuery = await getDoc(doc(db, "products", `${idProduct}`))
             return specificQuery.data()
-            }
+        }
 
         fetchFirestone()
             .then(result => setInfo(result))
             .catch(err => setError(err))
 
-
     }, [idProduct])
-
-
-    /* 
-    
-        const fetchFirestone = async () => {            
-            const queryCategory = query(collection(db, "products"), where("category", "==", `${idCategory}`));
-            const queryToDatabase = await getDocs(queryCategory)
-
-            const dataFirestone = queryToDatabase.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-
-            return dataFirestone
-        }
-    */
-
 
     if (error !== null) return  <p>Algo salio mal</p>
     

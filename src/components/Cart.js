7e4463c4db20e18e9ecabf6c646edbf58978187e
ezequiel <ps3ezequiel@gmail.com>
2022-05-08 17:03:23 +0200

    minor fixs

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -68,96 +68,117 @@ const Cart = () => {
             total: subTotal
         }
 
-        console.log(data)
-        console.log(order)
+        console.log("orden enviada a la DB:", order)
 
-        // const createOrder = async () => {
-        //     const orderRef = doc(collection(db, "orders"))
-        //     await setDoc(orderRef, order)
-        //     updateDB()
-        //     return orderRef;
-        // }
+        const createOrder = async () => {
+            const orderRef = doc(collection(db, "orders"))
+            await setDoc(orderRef, order)
+            updateDB()
+            return orderRef;
+        }
 
-        // createOrder()
-        // .then(result => Swal.fire(
-        //         'Order Created!',
-        //         `Your order was generated under the ID: 
-        //              ${result.id}`,
-        //         'success'
-        //     ))
+        createOrder()
+        .then(result => Swal.fire(
+                'Order Created!',
+                `Your order was generated under the ID: ${result.id}`,
+                'success'
+            ))
+            .then(context.removeAllItems())
+            .then(setModal((prevState) => !prevState))
 
-        //     .then(context.removeAllItems())
-        //     .then(setModal((prevState) => !prevState))
-
+            // NOTA: se puede revisar la orden enviada a la DB en la consola.
             
-        //     .catch(err => Swal.fire({
-        //         icon: 'error',
-        //         title: 'Oops...',
-        //         text: `Something went wrong! 
-        //             Error: ${err}` ,
-        //         footer: 'Please, try again!'
-        //     }))
+            .catch(err => Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `Something went wrong! 
+                    Error: ${err}` ,
+                footer: 'Please, try again!'
+            }))
     }
 
     return (
         <main className="flex justify-evenly min-h-[80vh] relative z-0">
 
             
-            <section className="flex flex-col justify-start items-start px-4 py-4 md:py-6 md:p-6 xl:p-8 w-8/12">
-                <p className="self-center text-lg md:text-xl font-semibold mb-6 leading-6 xl:leading-5 text-gray-800">Order Summary</p>
-                {
-                    context.cartList?.map((item) => {
+        <section className="flex flex-col justify-start items-start px-4 py-4 md:py-6 md:p-6 xl:p-8 w-8/12">
+            <p className="self-center text-lg md:text-xl font-semibold mb-6 leading-6 xl:leading-5 text-gray-800">Order Summary</p>
+            {
+                context.cartList?.map((item) => {
 
-                    let productCategory
+                let productCategory
 
-                    // Esto es solo para poder tener el texto con la primera letra en mayuscula
-                    if (item.category === "electronics") productCategory = "Electronics"
-                    if (item.category === "jewelery") productCategory = "Jewelery"
-                    if (item.category === "men's clothing") productCategory = "Men's clothing"
-                    if (item.category === "women's clothing") productCategory = "Women's clothing"
+                // Esto es solo para poder tener el texto con la primera letra en mayuscula
+                if (item.category === "electronics") productCategory = "Electronics"
+                if (item.category === "jewelery") productCategory = "Jewelery"
+                if (item.category === "men's clothing") productCategory = "Men's clothing"
+                if (item.category === "women's clothing") productCategory = "Women's clothing"
 
-                    return (
-                        <div key={`${item.id}-${item.size}-${item.color}`} className="mt-4 md:mb-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-                            <div className="flex justify-center items-center w-5/12 mx-auto my-6 md:my-0  md:w-28">
-                                <img className="w-9/12 md:block" src={item.image} alt={item.description} />
+
+                return (
+
+                <div key={`${item.id}-${item.size}-${item.color}`} className="mt-4 md:mb-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
+                    <div className="flex justify-center items-center w-5/12 mx-auto my-6 md:my-0  md:w-28">
+                        <img className="w-9/12 md:block" src={item.image} alt={item.description} />
+                    </div>
+                    <div className="border-b border-gray-300 md:flex-row flex-col flex justify-between items-start w-full pb-8 ">
+                        <div className="w-full flex flex-col justify-start items-center space-y-8">
+                            <Link to={`/products/${item.id}`}><h3 className="text-xl md:text-base xl:text-xl font-semibold leading-6 text-gray-800">{item.title}</h3></Link>
+                            <div className="flex justify-center items-center flex-row gap-4">
+                                <p className="text-sm leading-none text-gray-800"><span className="font-bold pr-1">Category: </span> {productCategory}</p>
+                                <p className="text-sm leading-none text-gray-800"><span className="font-bold pr-1">Size: </span> {item.size}</p>
+                                <p className="text-sm leading-none text-gray-800"><span className="font-bold pr-1">Color: </span> {item.color}</p>
+                                
                             </div>
-                            <div className="border-b border-gray-300 md:flex-row flex-col flex justify-between items-start w-full pb-8 ">
-                                <div className="w-full flex flex-col justify-start items-center space-y-8">
-                                    <Link to={`/products/${item.id}`}><h3 className="text-xl md:text-base xl:text-xl font-semibold leading-6 text-gray-800">{item.title}</h3></Link>
-                                    <div className="flex justify-center items-center flex-row gap-4">
-                                        <p className="text-sm leading-none text-gray-800"><span className="font-bold pr-1">Category: </span> {productCategory}</p>
-                                        <p className="text-sm leading-none text-gray-800"><span className="font-bold pr-1">Size: </span> {item.size}</p>
-                                        <p className="text-sm leading-none text-gray-800"><span className="font-bold pr-1">Color: </span> {item.color}</p>
-                                        
-                                    </div>
-                                </div>
-                                <div className="flex self-center justify-evenly xl:justify-around space-x-8 items-start w-full">
-                                    <p className="text-base xl:text-lg leading-6">$ {item.price} <span className="text-red-300 hidden xl:inline line-through pl-2"> $ {(item.price * 1.3).toFixed(2)}</span></p>
-                                    <p className="text-base xl:text-lg leading-6 text-gray-800">{item.quantity}</p>
-                                    <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">$ {Number(item.price) * Number(item.quantity)}</p>
-                                </div>
-                                <form onSubmit={(e) => context.removeItem(e)} data-id={item.id} data-color={item.color} data-size={item.size} data-identificator={item.identificator} className="self-center">
-                                    <button type="submit" className="text-gray-700 hover:text-red-500">
+                        </div>
+                        <div className="flex self-center justify-evenly xl:justify-around space-x-8 items-start w-full">
+                            <p className="text-base xl:text-lg leading-6">
+                                $ {item.price} 
+                                    <span className="text-red-300 hidden xl:inline line-through pl-2"> 
+                                        $ {(item.price * 1.3).toFixed(2)}
+                                    </span>
+                            </p>
+                            <p className="text-base xl:text-lg leading-6 text-gray-800">
+                                {item.quantity}
+                            </p>
+                            <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">
+                                $ {Number(item.price) * Number(item.quantity)}
+                            </p>
+                        </div>
+                        <form 
+                            onSubmit={(e) => context.removeItem(e)} 
+                            data-id={item.id} 
+                            data-color={item.color} 
+                            data-size={item.size} 
+                            data-identificator={item.identificator} 
+                            className="self-center">
+                                <button 
+                                    type="submit" 
+                                    className="text-gray-700 hover:text-red-500">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                         </svg>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    )})
-                }
-            </section>
-            <CartAside props={{
-                modal,
-                payment,
-                SHIPPING,
-                setData,
-                checkout,
-                subTotal,
-                setModal,
-                data
-            }}/>
+                                </button>
+                        </form>
+                    </div>
+                </div>
+                )})
+            }
+        </section>
+
+
+        <CartAside props={{
+            modal,
+            payment,
+            SHIPPING,
+            setData,
+            checkout,
+            subTotal,
+            setModal,
+            data
+        }}/>
+
+
         </main>
     )
 } 
--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -2,15 +2,21 @@
 
 const CartModal = ({props}) => {
 
-    const handleChange = (e) => props.setData({...props.data, [e.target.id]: e.target.value})
+    const {
+        payment,
+        setModal,
+        setData,
+        data
+    } = props
 
+    const handleChange = (e) => setData({...data, [e.target.id]: e.target.value})
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        props.payment()
+        payment()
     }
 
-    const close = () => props.setModal(prevState => !prevState)
+    const close = () => setModal(prevState => !prevState)
 
     return (
         <form onSubmit={(e) => handleSubmit(e)} className="z-10 absolute w-[30vw] text-left py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
@@ -19,101 +25,103 @@ const CartModal = ({props}) => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                 </svg>
             </div>
-            <h1 className="text-gray-800 text-center font-lg font-bold tracking-normal leading-tight mb-4">
-                Enter Billing Details
-            </h1>
 
-            <label htmlFor="name" className="w-full text-left text-gray-800 text-sm font-bold leading-tight tracking-normal">
-                Name
-            </label>
-            <input 
-                id="name" 
-                required 
-                value={props.data.name}
-                onChange={handleChange}
-                className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
-                placeholder="James" />
+            <h1 className="text-gray-800 text-center font-lg font-bold tracking-normal leading-tight mb-4">Enter Billing Details</h1>
 
-            <label htmlFor="email" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">
-                Email
-            </label>
-            <input 
-                id="email"
-                value={props.data.email}
-                onChange={handleChange}
-                className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
-                placeholder="James@gmail.com" />
-
-            <label htmlFor="phone" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">
-                Phone Number
-            </label>
-            <input 
-                id="phone" 
-                value={props.data.phone}
-                onChange={handleChange}
-                className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
-                placeholder="XXX-XXX-XXX" />
-
-            <label htmlFor="cardNumber" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">
-                Card Number
-            </label>
-            
-            <div className="relative mb-5 mt-2">
-                <div className="absolute text-gray-600 flex items-center px-4 border-r h-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                    </svg>
-                </div>
+            <label htmlFor="name" className="w-full text-left text-gray-800 text-sm font-bold leading-tight tracking-normal">Name</label>
                 <input 
-                    id="cardNumber" 
+                    id="name" 
                     required 
-                    value={props.data.cardNumber}
+                    value={data.name}
                     onChange={handleChange}
-                    className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-16 text-sm border-gray-300 rounded border" 
-                    placeholder="XXXX - XXXX - XXXX - XXXX" />
-            </div>
-            <label htmlFor="expiration" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">
-                Expiration Date
-            </label>
+                    className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
+                    placeholder="James" />
 
-            <div className="relative mb-5 mt-2">
-                <div className="absolute right-0 text-gray-600 flex items-center pr-3 h-full cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                    </svg>
-                </div>
+
+            <label htmlFor="email" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Email</label>
                 <input 
-                    id="expiration" 
-                    required 
-                    value={props.data.expiration}
+                    id="email"
+                    value={data.email}
                     onChange={handleChange}
-                    className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
-                    placeholder="MM/YY" />
-            </div>
+                    className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
+                    placeholder="James@gmail.com" />
+
+
+            <label htmlFor="phone" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Phone Number</label>
+                <input 
+                    id="phone" 
+                    value={data.phone}
+                    onChange={handleChange}
+                    className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
+                    placeholder="XXX-XXX-XXX" />
+
+            <label htmlFor="cardNumber" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Card Number</label>
+                <div className="relative mb-5 mt-2">
+                    <div className="absolute text-gray-600 flex items-center px-4 border-r h-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                        </svg>
+                    </div>
+                    <input 
+                        id="cardNumber" 
+                        required 
+                        value={data.cardNumber}
+                        onChange={handleChange}
+                        className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-16 text-sm border-gray-300 rounded border" 
+                        placeholder="XXXX - XXXX - XXXX - XXXX" />
+                </div>
+
+
+            <label htmlFor="expiration" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Expiration Date</label>
+
+                <div className="relative mb-5 mt-2">
+                    <div className="absolute right-0 text-gray-600 flex items-center pr-3 h-full cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
+                    </div>
+                    <input 
+                        id="expiration" 
+                        required 
+                        value={data.expiration}
+                        onChange={handleChange}
+                        className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
+                        placeholder="MM/YY" />
+                </div>
+
+
             <label htmlFor="cvc" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">CVC</label>
 
-            <div className="relative mb-5 mt-2">
-                <div className="absolute right-0 text-gray-600 flex items-center pr-3 h-full cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
+                <div className="relative mb-5 mt-2">
+                    <div className="absolute right-0 text-gray-600 flex items-center pr-3 h-full cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </div>
+                    <input 
+                        id="cvc" 
+                        required 
+                        value={data.cvc}
+                        onChange={handleChange}
+                        className="mb-8 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
+                        placeholder="XXX" />
                 </div>
-                <input 
-                    id="cvc" 
-                    required 
-                    value={props.data.cvc}
-                    onChange={handleChange}
-                    className="mb-8 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" 
-                    placeholder="XXX" />
-            </div>
 
             <div className="flex items-center justify-between w-full">
-                <button type="button" onClick={close} className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm">
+
+                <button 
+                    type="button" 
+                    onClick={close} 
+                    className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm">
                     Cancel
                 </button>
-                <button type="submit" className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">
+
+                <button 
+                    type="submit" 
+                    className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">
                     Pay
                 </button>
+
             </div>
         </form>
     )
